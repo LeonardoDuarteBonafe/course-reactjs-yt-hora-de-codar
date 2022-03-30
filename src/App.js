9a5991c30d622fdc711252a1a2e8215481d36c76
LeonardoDuarteBonafe <32874289+LeonardoDuarteBonafe@users.noreply.github.com>
2022-03-29 23:20:02 -0300

    Aula 4 - Criando components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const name = 'Matheus'
@@ -18,6 +19,7 @@ function App() {
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1,2)}</p>
       <img src={url} alt="Minha Imagem"/>
+      <HelloWorld/>
     </div>
   );
 }
